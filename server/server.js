66baa5d3f263324ec71e76d1e0002135d7c8a429
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-26 14:11:37 +0530

    NEW backend setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`listening on port :https://localhost:${PORT}`);
+});
