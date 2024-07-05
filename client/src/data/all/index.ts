de0b6c9a591e83c38cfd60c25b2a5f6e9be7a376
gamitej <gamitejpratapsingh998@gmail.com>
2024-07-05 21:37:02 +0530

    NEW : products done

--- client/src/data/all/index.ts
+++ client/src/data/all/index.ts
@@ -2,13 +2,78 @@ export const productOptions = [
   { id: 1, label: "UREA", value: "urea" },
   { id: 2, label: "Bareli", value: "bareli" },
   { id: 9, label: "Chari", value: "seed-chari" },
+
+  // pesticide products
   { id: 4, label: "Pesticide", value: "pesticide" },
   { id: 10, label: "Pesticide UPL", value: "pesticide-upl" },
+
+  // wheat products
   { id: 4, label: "Wheat 1010", value: "wheat-1010" },
   { id: 5, label: "Wheat 4037", value: "wheat-4037" },
   { id: 6, label: "Wheat DBW 187", value: "wheat-DBW-187" },
   { id: 7, label: "Wheat GW 273", value: "wheat-GW-273" },
   { id: 8, label: "Wheat JMD 4037", value: "wheat-JMD-4037" },
+
+  // dhaan products
+  { id: 9, label: "Dhaan Unnat 1718", value: "dhaan-unnat-1718" },
+  { id: 10, label: "Dhaan Unnat 1652", value: "dhaan-unnat-1652" },
+  { id: 11, label: "Dhaan Unnat 1121", value: "dhaan-unnat-1121" },
+  { id: 12, label: "Dhaan Unnat 1847", value: "dhaan-unnat-1847" },
+  { id: 13, label: "Dhaan kamadhagiri 1692", value: "dhaan-kamadhagiri-1692" },
+  { id: 14, label: "Dhaan kamadhagiri 1847", value: "dhaan-kamadhagiri-1847" },
+  { id: 15, label: "Dhaan proline 1885", value: "dhaan-proline-1885" },
+  { id: 16, label: "Dhaan star 1718", value: "dhaan-star-1718" },
+
+  // bajra products
+  { id: 17, label: "Bajra mahadev super 21", value: "bajra-mahadev-super-21" },
+  { id: 18, label: "Bajra mahadev 51M51", value: "bajra-mahadev-51M51" },
+  {
+    id: 19,
+    label: "Bajra crystal pro agro 9001",
+    value: "bajra-crystal-pro-agro-9001",
+  },
+  {
+    id: 20,
+    label: "Bajra naath super 27",
+    value: "bajra-naath-super-27",
+  },
+  {
+    id: 21,
+    label: "Bajra pioneer 86M90",
+    value: "bajra-pioneer-86M90",
+  },
+  {
+    id: 22,
+    label: "Bajra kamadgiri 9090",
+    value: "bajra-kamadgiri-9090",
+  },
+  {
+    id: 22,
+    label: "Bajra adv 963",
+    value: "bajra-adv-963",
+  },
+
+  // Juwar products
+  {
+    id: 23,
+    label: "Juwar kamadgiri tiger",
+    value: "juwar-kamadgiri-tiger",
+  },
+  {
+    id: 24,
+    label: "Juwar amarnath",
+    value: "juwar-amarnath",
+  },
+  {
+    id: 25,
+    label: "Juwar maha gujrat 296",
+    value: "juwar-maha-gujrat-296",
+  },
+  {
+    id: 26,
+    label: "Juwar deshi",
+    value: "juwar-deshi",
+  },
 ];
 
 export const weightTypeOptions = [
@@ -28,6 +93,32 @@ export const wheatCompanyOptions = [
   { id: 4, label: "LOKMAN", value: "lokman" },
   { id: 5, label: "VARDHAMAN", value: "vardhaman" },
   { id: 7, label: "OTHERS", value: "others" },
+];
+
+export const bajraCompanyOptions = [
+  { id: 1, label: "KAMADHAGIRI", value: "kamadhagiri" },
+  { id: 2, label: "PIONEER", value: "pioneer" },
+  { id: 3, label: "ADV", value: "adv" },
+  { id: 6, label: "NAATH", value: "naath" },
+  { id: 4, label: "CRYSTAL", value: "crystal" },
+  { id: 5, label: "MAHADEV", value: "mahadev" },
+  { id: 7, label: "OTHERS", value: "others" },
+];
+
+export const juwarCompanyOptions = [
+  { id: 1, label: "KAMADHAGIRI", value: "kamadhagiri" },
+  { id: 2, label: "AMARNATH", value: "amarnath" },
+  { id: 3, label: "MAHA GUJRAT", value: "maha-gujrat" },
+  { id: 4, label: "DESHI", value: "deshi" },
+  { id: 5, label: "OTHERS", value: "others" },
+];
+
+export const dhaanCompanyOptions = [
+  { id: 1, label: "UNNAT", value: "unnat" },
+  { id: 2, label: "KAMADHAGIRI", value: "kamadhagiri" },
+  { id: 3, label: "PROLINE", value: "proline" },
+  { id: 4, label: "STAR", value: "star" },
+  { id: 5, label: "OTHERS", value: "others" },
 ];
 
 export const bareliCompanyOptions = [
@@ -71,6 +162,9 @@ export const companyNameOptions: {
   pesticide: pesticideCompanyOptions,
   bareli: bareliCompanyOptions,
   seed: seedCompanyOptions,
+  juwar: juwarCompanyOptions,
+  bajra: bajraCompanyOptions,
+  dhaan: dhaanCompanyOptions,
 };
 
 // ===================== PARTY OPTIONS ================
